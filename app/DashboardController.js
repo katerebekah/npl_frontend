6d870bc186e2556a868ca89f5538cde409174389
JYA8246 <gerald.wakefield@healthtrustpg.com>
2016-10-14 12:11:58 -0500

    initial dashboard implementation

--- app/DashboardController.js
+++ app/DashboardController.js
@@ -6,8 +6,34 @@
     function DashboardController($scope, DashboardService){
         var viewModel = this;
 
+        $scope.clients = [];
+        $scope.currentReferral;
+        $scope.referralProgressOrComplete = "progress";
+        $scope.getAllClients = function(){
+            DashboardService.GetAllClients()
+                .then(function(response){
+                    $scope.clients = response.data;
+                })
+        }
+        $scope.getAllClients();
 
+        $scope.openConfirm = function(referral){
+            if(!referral.ServiceInProgress && !referral.ServiceRendered){
+                $scope.referralProgressOrComplete = "progress";
+                $scope.currentReferral = referral;
+            } else if( referral.ServiceInProgress && !referral.DateServiceRendered) {
+                $scope.referralProgressOrComplete = "complete";
+                $scope.currentReferral = referral;
+            } else {
+                $scope.referral = {};
+            }
+            
+        }
 
+        $scope.confirmReferral = function(){
+            var submitReferral = $scope.currentReferral;
+            DashboardService.UpdateReferrel(submitReferral);
+            $scope.currentReferral = {};
+        }
     }
-
 })();
