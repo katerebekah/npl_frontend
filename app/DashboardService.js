8e32ea7eec62d5654c3468cee34d151ded9f3c8a
Kate <katerebekah@gmail.com>
2016-10-14 09:24:37 -0500

    beginning dashboard service

--- app/DashboardService.js
+++ app/DashboardService.js
@@ -26,7 +26,42 @@
              return deferred.promise;    
         }
 
-        return{}
+        var GetAllClients = function(){
+             var deferred = $q.defer();
+             $http.get(serviceBase + '/client').then(function(response){
+                deferred.resolve(response);
+             }, function(err){
+                 deferred.reject(err);
+             });
+             return deferred.promise;    
+        }
+        
+        var GetAllEmployees = function(){
+             var deferred = $q.defer();
+             $http.get(serviceBase + '/employee').then(function(response){
+                deferred.resolve(response);
+             }, function(err){
+                 deferred.reject(err);
+             });
+             return deferred.promise;    
+        }
+
+        var GetAllServices = function(){
+             var deferred = $q.defer();
+             $http.get(serviceBase + '/service').then(function(response){
+                deferred.resolve(response);
+             }, function(err){
+                 deferred.reject(err);
+             });
+             return deferred.promise;    
+        }
+
+        return{
+            GetAllClients: GetAllClients,
+            GetAllEmployees: GetAllEmployees,
+            GetAllReferrals: GetAllReferrals,
+            GetAllServices: GetAllServices
+        }
     }
 
 })();
