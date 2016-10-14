8e975759d61fe696778014bf7ccbb7d3104f3ddf
Kate <katerebekah@gmail.com>
2016-10-14 10:24:16 -0500

    working on client form

--- app/HomeController.js
+++ app/HomeController.js
@@ -5,6 +5,27 @@
 
     function HomeController($scope){
         var viewModel = this;
+
+        viewModel.openClientForm = function(){
+
+        }
+
+        viewModel.clientForm = {
+            firstName: "",
+            lastName: "",
+            areaCode: "",
+            phoneNumber: "",
+            areaCode2: "",
+            phoneNumber2: "",
+            emailAddress: "",
+            streetName: "",
+            houseNumber: "",
+            zipCode: "",
+            notes: "",
+            serviceIdsRequested: ""
+        }
+
+
     }
 
 })();
