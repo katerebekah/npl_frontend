d0290ae96a8fa84fa92645fef57fd239e5c26547
Kate <katerebekah@gmail.com>
2016-10-14 11:27:11 -0500

    ugly client form working

--- app/HomeController.js
+++ app/HomeController.js
@@ -6,12 +6,20 @@
     function HomeController($scope, DashboardService){
         var viewModel = this;
 
-        viewModel.openClientForm = function(){
-            $('#clientFormModal').openModal();
+        //Load Services for form
+        viewModel.services = [];
+        viewModel.getAllServices = function(){
+            DashboardService.GetAllServices().then(function(response){
+                viewModel.services = response.data;
+            })   
         }
+        viewModel.getAllServices();
+
+        //Confirmation message
         viewModel.success = false;
         viewModel.successMessage = "Thanks for signing up for services."
 
+        //client form modal
         viewModel.clientForm = {
             firstName: "",
             lastName: "",
@@ -27,12 +35,44 @@
             serviceIdsRequested: ""
         }
 
+        viewModel.clearForm = function(){
+            viewModel.clientForm = {
+                firstName: "",
+                lastName: "",
+                areaCode: "",
+                phoneNumber: "",
+                areaCode2: "",
+                phoneNumber2: "",
+                emailAddress: "",
+                streetName: "",
+                houseNumber: "",
+                zipCode: "",
+                notes: "",
+                serviceIdsRequested: ""
+            }
+
+        }
+
+        //active slides in modal
+        viewModel.active = 1;
+        viewModel.changeSlide = function(slideNumber){
+            viewModel.active = slideNumber;
+        }
+
+        //Open Exit and Sumbit form from modal
+        viewModel.openClientForm = function(){
+            $('#clientFormModal').openModal();
+        }
         viewModel.exit = function(){
+            viewModel.clearForm();
+            viewModel.active = 1;
             $('#clientFormModal').closeModal();
         }
         viewModel.submit = function(){
             DashboardService.AddClient(viewModel.clientForm).then(function(response){
                 $('#clientFormModal').closeModal();
+                viewModel.clearForm();
+                viewModel.active = 1;
                 viewModel.success = true;
             });
         }
